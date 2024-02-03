9fa1501de33e707bf19d1f1fb5edaa81c36a80be
Илья <157880180+user937721@users.noreply.github.com>
2024-02-03 11:19:54 +0200

    v1.2

--- game.py
+++ game.py
@@ -1,12 +1,15 @@
 from pygame import *
 
+win_width = 900
+win_height = 700
 back = (200, 255, 255)
-window = display.set_mode((900, 700))
+window = display.set_mode((win_width, win_height))
 window.fill(back)
 
 clock = time.Clock()
 FPS = 60
 game = True
+speed = 10
 
 class GameSprite(sprite.Sprite) :
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
@@ -22,18 +25,20 @@ class GameSprite(sprite.Sprite) :
 class Player(GameSprite) :
     def update(self) :
         keys = key.get_pressed()
-        if keys[K_a] and self.rect.x > 5:
+        if keys[K_w] and self.rect.y > 5:
             self.rect.x -= self.speed
-        if keys[K_d] and self.rect.x < win_width - 80 :
+        if keys[K_s] and self.rect.y < win_width - 80 :
             self.rect.x += self.speed
-    def fire(self) :
-        bullet = Bullet('bullet.png', self.rect.centerx, self.rect.top, 15, 20, -15)
-        bullets.add(bullet)
+
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.x -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_width - 80 :
+            self.rect.x += self.speed
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-            
+
     display.update()
     clock.tick(FPS)
